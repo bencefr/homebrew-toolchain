45d28868fbe5db9ccfa33f1c4b6406c42ebd1137
Bence Frenyó <26139379+bencefr@users.noreply.github.com>
2021-02-16 12:55:23 +0100

    Fix offenses

--- Formula/cmake.rb
+++ Formula/cmake.rb
@@ -11,20 +11,11 @@ class Cmake < Formula
     strategy :github_latest
   end
 
-#  bottle do
-    # sha256 cellar: :any_skip_relocation, arm64_big_sur: "c242c56b2cfc3053a1b277eaa1d5fd6cf1e50c469fd5c5091d9bf7b475bbffb5"
-    # sha256 cellar: :any_skip_relocation, big_sur:       "a682d329ab579365215c7f409810e10b199df02d8856a760d472d4fb7c30bb42"
-    # sha256 cellar: :any_skip_relocation, catalina:      "4f33e84c319f1ded3f2c3b26e57c9727b3842b1a44863054c83158203ad52e75"
-    # sha256 cellar: :any_skip_relocation, mojave:        "375c91283a2b18b80c99b9e93c8344fe9351d39b87927e89449bbcbb1ca2c10b"
-#  end
+  uses_from_macos "ncurses"
 
-#   depends_on "sphinx-doc" => :build
-
-   uses_from_macos "ncurses"
-
-#   on_linux do
-#     depends_on "openssl@1.1"
-#   end
+  on_linux do
+    depends_on "openssl@1.1"
+  end
 
   # The completions were removed because of problems with system bash
 
@@ -41,23 +32,14 @@ class Cmake < Formula
       --datadir=/share/cmake
       --docdir=/share/doc/cmake
       --mandir=/share/man
-    #   --sphinx-build=#{Formula["sphinx-doc"].opt_bin}/sphinx-build
-    #   --sphinx-html
-    #   --sphinx-man
     ]
-    # on_macos do
-    #   args += %w[
-    #     --system-zlib
-    #     --system-bzip2
-    #     --system-curl
-    #   ]
-    # end
 
     system "./bootstrap", *args, "--", *std_cmake_args,
                                        "-DCMake_INSTALL_EMACS_DIR=#{elisp}"
     system "make"
     system "make", "install"
-    system "rm", "-rf", "#{prefix}/share/doc", "#{prefix}/share/cmake/Help"
+    rm_rf("#{share}/doc")
+    rm_rf("#{share}/cmake/Help")
   end
 
   test do
@@ -65,4 +47,3 @@ class Cmake < Formula
     system bin/"cmake", "."
   end
 end
-
